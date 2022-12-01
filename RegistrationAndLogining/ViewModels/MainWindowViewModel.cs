a6bbdc28f2f1e30295726a90d682985d57d56822
Egorff <wrestler000ua@gmail.com>
2022-12-01 21:09:10 +0300

    I have started ViewModels

---
+++ RegistrationAndLogining/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using RegistrationAndLogining.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModelBaseLib.VM;
+
+namespace RegistrationAndLogining.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        #region Fields
+
+        object m_FrameContent;
+
+        #endregion
+        
+
+
+        #region Prop
+
+        public object FrameContent { get => m_FrameContent; set => Set(ref m_FrameContent, value); }
+
+        #endregion
+
+
+
+        #region Ctor
+
+        public MainWindowViewModel()
+        {
+            #region InitFields
+
+            m_RegPage = new RegistrationPage();
+
+            m_LoginPage = new LoginPage();
+
+            m_FrameContent = new object();
+
+            m_FrameContent = m_RegPage;
+
+            #endregion
+        }
+
+        #endregion
+
+
+
+        #region Methods
+
+        #endregion
+
+
+
+        #region Pages
+
+        RegistrationPage m_RegPage;
+
+        LoginPage m_LoginPage;
+
+        #endregion
+    }
+}
