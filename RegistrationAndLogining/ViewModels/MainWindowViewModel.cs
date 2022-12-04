93c3c67ee86868f70924953ebcedd8d7d2dd2224
Egorff <wrestler000ua@gmail.com>
2022-12-04 10:30:29 +0300

    page switching done

--- RegistrationAndLogining/ViewModels/MainWindowViewModel.cs
+++ RegistrationAndLogining/ViewModels/MainWindowViewModel.cs
@@ -2,15 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ViewModelBaseLib.Command;
 using ViewModelBaseLib.VM;
 
 namespace RegistrationAndLogining.ViewModels
 {
+    enum LoginRegistration : byte
+    {
+        register = 0, login
+    }
+
     public class MainWindowViewModel : ViewModelBase
     {
         #region Fields
+
+        LoginRegistration m_loginRegistration;
+
+        string m_loginButtonContent;
+
+        string m_title;
+
+        string m_message;
 
         object m_FrameContent;
 
@@ -21,6 +37,12 @@ namespace RegistrationAndLogining.ViewModels
         #region Prop
 
         public object FrameContent { get => m_FrameContent; set => Set(ref m_FrameContent, value); }
+
+        public string Title { get => m_title; set => Set(ref m_title, value); }
+
+        public string Message { get => m_message; set => Set(ref m_message, value); }
+
+        public string LoginButtonContent { get => m_loginButtonContent; set => Set(ref m_loginButtonContent, value); }
 
         #endregion
 
@@ -40,6 +62,22 @@ namespace RegistrationAndLogining.ViewModels
 
             m_FrameContent = m_RegPage;
 
+            m_title = "Register";
+
+            m_message = "If you already have an account, please sign in.";
+
+            m_loginButtonContent = "Login";
+
+            m_loginRegistration = LoginRegistration.register;
+
+            #endregion
+
+            #region Init commands
+
+            OnLoginSwitchButtonPressed = new Command(OnLoginSwitchButtonPressedExecute, CanOnLoginSwitchButtonPressedExecute);
+
+            
+
             #endregion
         }
 
@@ -48,6 +86,49 @@ namespace RegistrationAndLogining.ViewModels
 
 
         #region Methods
+
+        #region OnLoginSwitchButtonPressed
+
+        bool CanOnLoginSwitchButtonPressedExecute(object p)
+        {
+            return true;
+        }
+
+        void OnLoginSwitchButtonPressedExecute(object p)
+        {
+            switch (m_loginRegistration)
+            {
+                case LoginRegistration.register:
+
+                    Title = "Login";
+
+                    Message = "If you don`t have an account, please register.";
+
+                    LoginButtonContent = "Register";
+
+                    FrameContent = m_LoginPage;
+
+                    m_loginRegistration = LoginRegistration.login;
+
+                    break;
+
+                case LoginRegistration.login:
+
+                    FrameContent = m_RegPage;
+
+                    Title = "Register";
+
+                    Message = "If you already have an account, please sign in.";
+
+                    LoginButtonContent = "Login";
+
+                    m_loginRegistration = LoginRegistration.register;
+
+                    break;
+            }
+        }
+
+        #endregion
 
         #endregion
 
@@ -58,6 +139,14 @@ namespace RegistrationAndLogining.ViewModels
         RegistrationPage m_RegPage;
 
         LoginPage m_LoginPage;
+
+        #endregion
+
+
+
+        #region Commands
+
+        public ICommand OnLoginSwitchButtonPressed { get; set; }
 
         #endregion
     }
---
+++ ViewModelBaseLib/Command/Command.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModelBaseLib.Command.CommandBase;
+
+namespace ViewModelBaseLib.Command
+{
+    public class Command : CommandBaseClass
+    {
+        #region Fields
+
+        Action<object> m_Execute;
+
+        Func<object, bool> m_CanExecute;
+
+        #endregion
+
+
+        #region Prop
+
+        #endregion
+
+
+        #region Ctor
+
+        public Command(Action<object> execute, Func<object, bool> canExecute) 
+        {
+            m_CanExecute = canExecute;
+
+            m_Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public override bool CanExecute(object? parameter)
+        {
+            return m_CanExecute?.Invoke(parameter) ?? true;
+
+
+        }
+
+        public override void Execute(object? parameter)
+        {
+            m_Execute(parameter);
+        }
+
+        #endregion
+
+        
+
+
+    }
+}
