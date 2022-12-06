4ff68f9d1481a86389a0e37c4aefb617bd5aa5cb
Egorff <wrestler000ua@gmail.com>
2022-12-06 19:34:25 +0300

    User class has done

---
+++ DataBase/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Models
+{
+    public class User
+    {
+        #region Prop
+
+        [Required(ErrorMessage = "Property id mustn`t be null.")]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Property login mustn`t be null.")]
+        public string Login { get; set; }
+
+        public string NormalizedLogin { get; set; }
+
+        [Required(ErrorMessage = "Property e-mail mustn`t be null.")]
+        public string Email { get; set; }
+
+        public string NormalizedEmail { get; set; }
+
+        [MaxLength(20, ErrorMessage = "max length = 20")]
+        [MinLength(10, ErrorMessage = "min length = 10")]
+        [Required(ErrorMessage = "Property password mustn`t be null.")]
+        public string Password { get; set; }
+
+        #endregion
+
+        #region Ctor
+
+        public User(Guid id, string login, string email, string password)
+        {
+            Id = id;   
+            Login = login;
+            NormalizedLogin = login.ToUpper();
+            Email = email;
+            NormalizedEmail = email.ToUpper();
+            Password = password;
+        }
+
+        public User()
+        {
+
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{NormalizedLogin}, {NormalizedEmail}.";
+        }
+    }
+}
--- RegistrationAndLogining/obj/Debug/net6.0-windows/View/Pages/RegistrationPage.g.i.cs
+++ RegistrationAndLogining/obj/Debug/net6.0-windows/View/Pages/RegistrationPage.g.i.cs
@@ -53,7 +53,7 @@ namespace RegistrationAndLogining.View.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RegistrationAndLogining;component/view/pages/registrationpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RegistrationAndLogining;V1.0.0.0;component/view/pages/registrationpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\Pages\RegistrationPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
